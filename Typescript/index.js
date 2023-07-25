60fa1d4ee69198ede1c4c852011017c7a1196611
Sseo Jjam <sjh960815@gmail.com>
2023-07-25 23:26:34 +0900

    fix: TIL_230725 내용추가

--- Typescript/index.js
+++
@@ -1,9 +0,0 @@
-var person = { name: 'kim', age: 20 };
-var 인간 = { name: 'kim', phone: 123, email: 'abc@naver.com' };
-var 회원가입정보 = {
-    name: 'kim',
-    phone: 123,
-    email: 'abc@naver.com',
-    미성년자: true,
-};
-//# sourceMappingURL=index.js.map
