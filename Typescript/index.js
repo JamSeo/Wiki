0525ed714a4df142b351c0a830232ba62346db4d
Sseo Jjam <sjh960815@gmail.com>
2023-08-01 23:14:51 +0900

    feat: TIL_230801

--- Typescript/index.js
+++
@@ -1,6 +0,0 @@
-var 링크들 = document.querySelectorAll('.naver');
-링크들.forEach(function (링크) {
-    if (링크 instanceof HTMLAnchorElement) {
-        링크.href = 'https://kakao.com';
-    }
-});
--- Typescript/index.ts
+++
@@ -1,7 +0,0 @@
-let 링크들 = document.querySelectorAll('.naver')
-
-링크들.forEach((링크) => {
-    if (링크 instanceof HTMLAnchorElement){
-        링크.href = 'https://kakao.com'
-    }
-})
