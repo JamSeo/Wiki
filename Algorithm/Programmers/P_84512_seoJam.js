e226596c80915d9ac7cc7e96367a9b1d4e62bf00
서재현 <sjh960815@naver.com>
2023-10-20 17:16:55 +0900

    refactor: 프로그래머스 '모음사전' 리팩토링

--- Algorithm/Programmers/P_84512_seoJam.js
+++ Algorithm/Programmers/P_84512_seoJam.js
@@ -1,31 +1,29 @@
+// 조합을 만드는 함수
+function generateCombinations(base) {
+  const newCombinations = [];
+  ["A", "E", "I", "O", "U"].forEach((char) => {
+    newCombinations.push(base + char);
+  });
+  return newCombinations;
+}
+
 function solution(word) {
-  const AEIOU = ["A", "E", "I", "O", "U"];
+  const MAX_LENGTH = 5;
+  let dictionary = ['0'];
 
-  const arr1 = AEIOU;
+  // 초기값 설정
+  let currentWords = ["A", "E", "I", "O", "U"];
 
-  const arr2 = [];
-  arr1.forEach((el) => {
-    AEIOU.forEach((str) => arr2.push(el + str))
-  })
-
-  const arr3 = [];
-  arr2.forEach((el) => {
-    AEIOU.forEach((str) => arr3.push(el + str))
-  })
-
-  const arr4 = [];
-  arr3.forEach((el) => {
-    AEIOU.forEach((str) => arr4.push(el + str))
-  })
-
-  const arr5 = [];
-  arr4.forEach((el) => {
-    AEIOU.forEach((str) => arr5.push(el + str))
-  })
-
-  const dictionary = ['0', ...arr1, ...arr2, ...arr3, ...arr4, ...arr5].sort();
-
-  for (let idx = 0; idx < dictionary.length; idx++) {
-    if (word === dictionary[idx]) return idx
+  // 최대 길이가 될 때까지 조합 생성
+  for (let i = 0; i < MAX_LENGTH; i++) {
+    dictionary = dictionary.concat(currentWords);
+    let temp = [];
+    currentWords.forEach((word) => {
+      temp = temp.concat(generateCombinations(word));
+    });
+    currentWords = temp;
   }
+
+  // word의 위치 찾기
+  return dictionary.sort().indexOf(word)
 }
